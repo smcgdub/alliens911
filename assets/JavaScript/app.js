8c3dec0a1386cc0aa35ffa4ebaaf1e6f8544d5f6
Stephen Mc Govern <stephen_xyz1@hotmail.com>
2022-03-28 16:55:28 +0100

    Added the JS function to ufo search page so search results are now being displayed after search

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,2 +1,32 @@
 // Photo gallery JS
 baguetteBox.run('.cards-gallery', {animation: 'slideIn'});
+
+// UFO Search Function
+function callArchiSearch() {
+
+  // countryLat = document.getElementById('lat').value;
+  // countryLong = document.getElementById('long').value;
+
+  let searchLocation = document.getElementById('location_search_input').value;
+  let pwd = document.getElementById('pwd').value;
+  let urlCoordinates = searchLocation;
+
+  // let urlCoordinates = countryLat + '%2C' + countryLong;
+
+  // search_location
+  let urlParameters = "&search_range=10000&pwd=" + pwd + "&SearchType=freesearch" + "&keywords=ufo";
+
+  // let urlCoordinates = 53.528680 + '%2C' + -2.123737;
+
+  console.log(urlCoordinates);
+
+  // window.open("http://localhost:8080/login?cid="+ myu +"&pwd="+ myp ,"MyTargetWindowName");
+  window.open(
+    "https://www.archiuk.com/cgi-bin/archi_new_search_engine.pl?search_location=" + urlCoordinates + urlParameters
+
+    // "https://www.archiuk.com/cgi-bin/web-archi.pl?ARCHIFormFreeSearch=${urlCoordinates}&SearchType=freesearch&distance=10000"
+
+
+    // "https://www.archiuk.com/placenames"
+  );
+}
